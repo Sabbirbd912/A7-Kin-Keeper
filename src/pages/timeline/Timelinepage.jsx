5199d55bfe065afa4a3d1d952886e9890911ae75
Sabbir Ahmed <sabbirbd912@gmail.com>
2026-04-17 09:23:27 +0600

    filter added

--- src/pages/timeline/Timelinepage.jsx
+++ src/pages/timeline/Timelinepage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { use, useContext, useState } from "react";
 import { CalledFriendContext } from "../../context/CalledFriendProvider";
 import { IoCallOutline, IoVideocamOutline } from "react-icons/io5";
 import { BsChatLeftText } from "react-icons/bs";
@@ -6,10 +6,16 @@ import { BsChatLeftText } from "react-icons/bs";
 const Timelinepage = () => {
   const { call } = useContext(CalledFriendContext);
 
+  const [filter, setFilter] = useState("All");
+
   const getIcon = (type) => {
     switch (type) {
       case "Call":
-        return <span className="text-yellow-500 text-2xl"><IoCallOutline /></span>; 
+        return (
+          <span className="text-yellow-500 text-2xl">
+            <IoCallOutline />
+          </span>
+        );
       case "Text":
         return <BsChatLeftText className="text-gray-400" size={24} />;
       case "Video":
@@ -19,15 +25,45 @@ const Timelinepage = () => {
     }
   };
 
+  const filteredCalls =
+    filter === "All"
+      ? call
+      : call.filter((item) => item.interactionType === filter);
+
   return (
     <div className="container mx-auto p-6 max-w-4xl">
       <h2 className="text-4xl font-bold text-slate-800 mb-8">Timeline</h2>
 
+      <div className="mb-4">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1 bg-white border-gray-300">
+            {filter==="All" ? "Filter Interactions" :  `Showing: ${filter}`}
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a onClick={() => setFilter("All")}>All</a>
+            </li>
+            <li>
+              <a onClick={() => setFilter("Call")}>Calls</a>
+            </li>
+            <li>
+              <a onClick={() => setFilter("Text")}>Texts</a>
+            </li>
+            <li>
+              <a onClick={() => setFilter("Video")}>Videos</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <div className="flex flex-col gap-4">
-        {call.length === 0 ? (
+        {filteredCalls.length === 0 ? (
           <p className="text-gray-500 italic">No interactions yet.</p>
         ) : (
-          call.map((item, index) => (
+          filteredCalls.map((item, index) => (
             <div
               key={index}
               className="flex items-center gap-4 bg-white p-5 rounded-xl border border-gray-100 shadow-sm hover:shadow-md transition-shadow"
@@ -37,7 +73,7 @@ const Timelinepage = () => {
               </div>
 
               <div>
-                <p className="text-gray-800"> 
+                <p className="text-gray-800">
                   <span className="text-gray-500 ml-1 font-medium">
                     {item.interactionType} with {item.name}
                   </span>
